334a72205c9782ece1cf7887a091c0043b6dab5a
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-03 17:15:48 +0530

    EWrwer

--- project/src/rabbitmq_broker.erl
+++ project/src/rabbitmq_broker.erl
@@ -3,7 +3,7 @@
 
 -behaviour(gen_server).
 -export([start_link/1, start/1, stop/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {
     channel, supervisor, connection, queue_supervisor
