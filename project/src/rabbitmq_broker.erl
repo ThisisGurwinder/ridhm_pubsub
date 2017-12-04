d9973923461ebd567c3e771a71bb1c5ea65e2a19
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-04 13:25:19 +0530

    werwer

--- project/src/rabbitmq_broker.erl
+++ project/src/rabbitmq_broker.erl
@@ -68,8 +68,8 @@ handle_cast(_Message, State) ->
     {noreply, State}.
 
 terminate(_Reason, #state{channel = Channel, connection = Connection}) ->
-    amqp_channel:close(Channel),
-    amqp_connection:close(Connection),
+    % amqp_channel:close(Channel),
+    % amqp_connection:close(Connection),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
